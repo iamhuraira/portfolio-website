61fa9bac58c47bca981a272776a0c7201ad43923
Abu Huraira <iamhuraira429@gmail.com>
2022-06-04 00:35:09 +0500

    Creatinh Hero Component

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='container'>
+        <div>
+            <p>
+                Hello, I'm Anjum Hussain
+            </p>
+            <h1>Wordpress Developer </h1>
+            <p>Complete Website Build with Elementor Pro.</p>
+        </div>
+    </div>
+  )
+}
+
+export default Hero;
