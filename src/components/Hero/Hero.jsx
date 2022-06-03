4e0d5e5f9a27503191afd5e961c58a6af57ecd51
Abu Huraira <iamhuraira429@gmail.com>
2022-06-04 01:53:42 +0500

    Done Hero Commponent

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,17 +1,35 @@
-import React from 'react'
+import React from 'react';
+import './style.css';
+import me from '../../assets/images/3-removebg.png';
+import Button from '../UI/Button';
 
 const Hero = () => {
   return (
-    <div className='container'>
+    <div className="container" style={{ marginTop: '70px' }}>
+      <div className="flex-row justify-content align-center">
         <div>
-            <p>
-                Hello, I'm Anjum Hussain
-            </p>
-            <h1>Wordpress Developer </h1>
-            <p>Complete Website Build with Elementor Pro.</p>
+          <p className="uppercase bold-500 textColor ls-1 mtb-10">
+            <span className="primaryColor"> Hello,</span> I'm Anjum Hussain
+          </p>
+          <h1 className="textColor ls-1 mtb-10">Wordpress Developer </h1>
+          <p className="font-12 grey mtb-10">
+            Complete Website Build with Elementor Pro.
+          </p>
+          <div className="flex-row " style={{margin:'30px 0'}}>
+            <div>
+              <Button label="Hire Me" />
+            </div>
+            <div className="mlr-10 ">
+              <Button label="Download CV" inverse={true} />
+            </div>
+          </div>
         </div>
+        <div className="meRightimgcontainer">
+          <img src={me} alt="" />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Hero;
